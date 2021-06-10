89f62dbacddf756db09eb64a816d104801d1132d
Anu Upadhyay <anu.upadhyay.1994@gmail.com>
2021-06-10 13:45:15 +0530

    changes in settings.py file

--- backend/settings.py
+++ backend/settings.py
@@ -170,7 +170,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
